996bbf9b5140561367e9cf440b29b353eac747cf
Dylan Maryk <dylan@door2door.io>
2018-01-27 17:40:52 +0100

    Set dotted polyline on map view

--- DottedPolyline/AppDelegate.swift
+++ DottedPolyline/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 dylanmaryk. All rights reserved.
 //
 
+import GoogleMaps
 import UIKit
 
 @UIApplicationMain
@@ -15,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyAp8qUevlYh0vbn1mWa7I5ewbcQj4acFTY")
         return true
     }
 
--- DottedPolyline/ViewController.swift
+++ DottedPolyline/ViewController.swift
@@ -6,20 +6,60 @@
 //  Copyright © 2018 dylanmaryk. All rights reserved.
 //
 
-import UIKit
+import GoogleMaps
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, GMSMapViewDelegate {
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    private let polyline = GMSPolyline(path: GMSPath(fromEncodedPath: "{dq_IowzpAzkAbhF"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        set(polyline: polyline, on: mapView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func mapView(_ mapView: GMSMapView, didChange position: GMSCameraPosition) {
+        set(polyline: polyline, on: mapView)
     }
-
-
+    
+    private func set(polyline: GMSPolyline, on mapView: GMSMapView) {
+        guard let path = polyline.path else {
+            return
+        }
+        mapView.clear()
+        let intervalDistanceIncrement: CGFloat = 10
+        let circleRadiusScale = 1 / mapView.projection.points(forMeters: 1, at: mapView.camera.target)
+        var previousCircle: GMSCircle?
+        for coordinateIndex in 0 ..< path.count() - 1 {
+            let startCoordinate = path.coordinate(at: coordinateIndex)
+            let endCoordinate = path.coordinate(at: coordinateIndex + 1)
+            let startLocation = CLLocation(latitude: startCoordinate.latitude, longitude: startCoordinate.longitude)
+            let endLocation = CLLocation(latitude: endCoordinate.latitude, longitude: endCoordinate.longitude)
+            let pathDistance = endLocation.distance(from: startLocation)
+            let intervalLatIncrement = (endLocation.coordinate.latitude - startLocation.coordinate.latitude) / pathDistance
+            let intervalLngIncrement = (endLocation.coordinate.longitude - startLocation.coordinate.longitude) / pathDistance
+            for intervalDistance in 0 ..< Int(pathDistance) {
+                let intervalLat = startLocation.coordinate.latitude + (intervalLatIncrement * Double(intervalDistance))
+                let intervalLng = startLocation.coordinate.longitude + (intervalLngIncrement * Double(intervalDistance))
+                let circleCoordinate = CLLocationCoordinate2D(latitude: intervalLat, longitude: intervalLng)
+                if let previousCircle = previousCircle {
+                    let circleLocation = CLLocation(latitude: circleCoordinate.latitude,
+                                                    longitude: circleCoordinate.longitude)
+                    let previousCircleLocation = CLLocation(latitude: previousCircle.position.latitude,
+                                                            longitude: previousCircle.position.longitude)
+                    if mapView.projection.points(forMeters: circleLocation.distance(from: previousCircleLocation),
+                                                 at: mapView.camera.target) < intervalDistanceIncrement {
+                        continue
+                    }
+                }
+                let circleRadius = 3 * CLLocationDistance(circleRadiusScale)
+                let circle = GMSCircle(position: circleCoordinate, radius: circleRadius)
+                circle.map = mapView
+                previousCircle = circle
+            }
+        }
+    }
+    
 }
-
